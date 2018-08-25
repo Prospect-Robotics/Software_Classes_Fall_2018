2d654479fd22ce74421922249402c94cb76d506f
Grady Whelan <grady.whelan@gmail.com>
2018-08-25 12:59:35 -0700

    Student code to modify

--- src/Higher_or_Lower.java
+++ src/Higher_or_Lower.java
@@ -5,25 +5,19 @@ public class Higher_or_Lower {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		Random random = new Random();
-		int randomNumber = random.nextInt(101);
+		//Line 9: Make a variable and set it to random.nextInt(101)
 		
-		System.out.println("Please enter a number between 1 and 100");
-		boolean hasNumberBeenGuessed = false;
-		while(hasNumberBeenGuessed == false) {
+		//Line 11: Tell the user to guess a number between 1 and 100
+		
+		//Line 13: Create a variable to keep track of whether they've won yet or not
+
+		while(/*Insert Something Here!*/) {
 			int input = scanner.nextInt();
-			if (input == randomNumber) {
-				hasNumberBeenGuessed = true;
-			}
-			else {
-				if (input > randomNumber) {
-					System.out.println("Too High!");
-				}
-				else {
-					System.out.println("Too Low!");
-				}
-			}
+			/*Write code to check whether the number is higher or lower than random number
+			or if it has been guessed, and tell the user if it is higher or lower*/
+			
 		}
-		System.out.println("Number Guessed");
+		//Tell the user they've won!
 		
 	}
 }
